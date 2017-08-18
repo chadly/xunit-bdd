686847f77b05d7cdf151d84e6647634376f97aec
Daniel Tullemans <daniel.tullemans@civicsource.com>
2017-08-18 16:41:30 -0500

    Remove obsolete ObservationCommand

--- xUnit.BDD/ObservationAttribute.cs
+++ xUnit.BDD/ObservationAttribute.cs
@@ -8,21 +8,7 @@ namespace Xunit.Extensions
 	/// <summary>
 	/// Identifies a method as an observation which asserts the specification
 	/// </summary>
-	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
 	public class ObservationAttribute : FactAttribute
 	{
-		protected override IEnumerable<ITestCommand> EnumerateTestCommands(IMethodInfo method)
-		{
-			foreach (ITestCommand command in EnumerateTestCommandsInternal(method))
-			{
-				yield return new ObservationCommand(command);
-			}
-		}
-
-		private IEnumerable<ITestCommand> EnumerateTestCommandsInternal(IMethodInfo method)
-		{
-			//this extra method is to avoid compiler warning
-			return base.EnumerateTestCommands(method);
-		}
 	}
 }
