4cc816aff3b4d14586669b611b25157c01959d5d
Chad Lee <chad.lee@archoninfosys.com>
2017-08-22 12:12:31 -0500

    Remove legacy batch file

--- pack.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe xunit.bdd.sln /p:Configuration=Release /nologo
-nuget pack xunit.bdd\xunit.bdd.csproj -Prop Configuration=Release
-pause
